b2c7812e93e4edc191db3e84c6cac2d9e2ae92a4
ruslankus <ruslankus@yahoo.com>
2014-07-09 15:39:53 +0300

    Added debug function

--- protected/helpers/Debug.php
+++ protected/helpers/Debug.php
@@ -29,6 +29,22 @@ class Debug
             echo "</pre>";
         }
     }
+    
+    
+    /**
+     * Функция отладки
+     * 
+     * @param $value параметр что отлаживаем
+     */
+    public function d($value = null,$die = 1){
+        echo 'Debug: <br/><pre>';
+        print_r($value);
+        echo '</pre>';
+        
+        if($die)
+            die;
+    } 
+
 }
 
 ?>
