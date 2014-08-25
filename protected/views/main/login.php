c4d92bf2ffdb94506474cf179330392ee0d5131e
Alex V <darkoffalex@yandex.ru>
2014-08-25 11:29:10 +0300

    password field fixed

--- protected/views/main/login.php
+++ protected/views/main/login.php
@@ -19,7 +19,7 @@
                     </div>
                     <div class="form-group">
                         <?php echo $form->label($model,'password');?>
-                        <?php echo $form->textField($model,'password',array('class'=>'form-control'));?>
+                        <?php echo $form->passwordField($model, 'password', array('class' => 'form-control')); ?>
                         <?php echo $form->error($model,'password'); ?>
                     </div>
                     <button class="btn btn-default pull-right"><span><?php echo $this->labels['login']; ?></span><span><img src="/images/filters_arrow.png" width="36" height="36" ></span></button>
